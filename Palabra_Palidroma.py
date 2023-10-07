7fe561cf77185dbefe6fa9778a723682100e459b
Jparedest1 <jhony.paredes1994@gmail.com>
2023-10-07 11:51:28 -0600

    Se agrego la funcion de validar si es palabra aguda.

--- Palabra_Palidroma.py
+++ Palabra_Palidroma.py
@@ -20,3 +20,30 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+def es_palabra_aguda(palabra):
+    # Convierte la palabra a minúsculas para facilitar la comparación
+    palabra = palabra.lower()
+    
+    # Define una lista de vocales acentuadas
+    vocales_acentuadas = ['á', 'é', 'í', 'ó', 'ú']
+    
+    # Verifica si la última letra de la palabra es una vocal acentuada
+    if palabra[-1] in vocales_acentuadas:
+        return True
+    else:
+        return False
+
+# Ejemplos de uso:
+palabra1 = "canción"
+palabra2 = "computadora"
+
+if es_palabra_aguda(palabra1):
+    print(f'La palabra "{palabra1}" es aguda.')
+else:
+    print(f'La palabra "{palabra1}" no es aguda.')
+
+if es_palabra_aguda(palabra2):
+    print(f'La palabra "{palabra2}" es aguda.')
+else:
+    print(f'La palabra "{palabra2}" no es aguda.')
