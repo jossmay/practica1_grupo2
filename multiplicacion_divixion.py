d370ee69efeb1c2573e5246055e64fe7ffba9d90
JRonaldo79 <jronaldoma@gmail.com>
2023-10-07 20:09:39 -0600

    Se agrego una funcionalidad extra

--- multiplicacion_divixion.py
+++ multiplicacion_divixion.py
@@ -3,6 +3,8 @@ def multiplicar(a, b):
     return a * b
 
 # Función para realizar la división
+
+
 def dividir(a, b):
     try:
         resultado = a / b
@@ -10,12 +12,21 @@ def dividir(a, b):
     except ZeroDivisionError:
         return "Error: No se puede dividir entre cero."
 
+# Función para convertir grados Fahrenheit a grados Celsius
+
+
+def fahrenheit_a_celsius(fahrenheit):
+    celsius = (fahrenheit - 32) * 5.0/9.0
+    return celsius
+
+
 while True:
     # Mostrar el menú
     print("Selecciona una operación:")
     print("1. Multiplicación")
     print("2. División")
-    print("3. Salir")
+    print("3. Convertir Fahrenheit a Celsius")
+    print("4. Salir")
 
     # Solicitar la elección del usuario
     opcion = input("Ingresa el número de la operación que deseas realizar: ")
@@ -31,12 +42,18 @@ while True:
         # División
         num1 = float(input("Ingresa el primer número: "))
         num2 = float(input("Ingresa el segundo número: "))
-	if num2 == 0:
-        	print("¡Cuidado! Estás dividiendo entre cero.")
-    	else:
-       		resultado = dividir(num1, num2)
-		print("El resultado de la división es:", resultado)
+        if num2 == 0:
+            print("¡Cuidado! Estás dividiendo entre cero.")
+        else:
+            resultado = dividir(num1, num2)
+            print("El resultado de la división es:", resultado)
     elif opcion == '3':
+        # Conversión de Fahrenheit a Celsius
+        fahrenheit = float(
+            input("Ingresa la temperatura en grados Fahrenheit: "))
+        celsius = fahrenheit_a_celsius(fahrenheit)
+        print("La temperatura en grados Celsius es:", celsius)
+    elif opcion == '4':
         # Salir del programa
         print("Saliendo.....")
         break
